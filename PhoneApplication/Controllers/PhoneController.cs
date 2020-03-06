833c68bf0d2837335f8d8da9101268e38876d086
abhijeetrana318@gmail.com <abhijeetrana318@gmail.com>
2020-03-05 20:20:18 -0500

    Add project files.

---
+++ PhoneApplication/Controllers/PhoneController.cs
@@ -0,0 +1,35 @@
+﻿using PhoneApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        [NonAction]
+        public List<Phone> GetPhoneList()
+        {
+            return new List<Phone>
+            {
+                new Phone
+                {
+                    PhoneID = 1,
+                    PhoneName = "Iphone11 Pro Max",
+                    MSRP = 100,
+                    ScreenSize = 20.2,
+                    DateReleased = DateTime.Now,
+                    Manufacturer = "Apple"
+                },
+            };
+        }
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var Phone = from p in GetPhoneList() orderby p.PhoneID select p;
+            return View(Phone);
+        }
+    }
+}
---
+++ PhoneApplication/Models/Phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            this.DateReleased = DateTime.Now;
+            this.PhoneName = "";
+        }
+
+        [Key]
+        public int PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public int MSRP { get; set; }
+        public double ScreenSize { get; set; }
+        public DateTime DateReleased { get; set; }
+        public String Manufacturer { get; set; }
+    }
+}
